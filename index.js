bb6627b5321a13e07ac1f60b891d10dfd26b66eb
Joseph Donkoh <93696965+JosephDonkoh@users.noreply.github.com>
2022-05-10 13:48:30 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-ocument.querySelector('.rules-btn').addEventListener('click', (e) =>{
+document.querySelector('.rules-btn').addEventListener('click', (e) =>{
     e.preventDefault();
     document.querySelector('.rules-popup').classList.toggle('rules-popup--active');
     document.querySelector('.overlay').classList.toggle('overlay--active');
