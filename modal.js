aea19b3d7c3a0e9777e3101199d26379f0e85fbc
Joseph Donkoh <93696965+JosephDonkoh@users.noreply.github.com>
2022-05-13 12:22:44 +0100

    Update modal.js

--- modal.js
+++ modal.js
@@ -4,3 +4,13 @@ const DOMStrings = {
   ruleBox: ".rules-overlay",
   closeBtn: ".cross-btn",
 };
+
+//fade rules box
+document.querySelector(DOMStrings.rules).addEventListener("click", () => {
+  document.querySelector(DOMStrings.ruleBox).classList.add("active");
+});
+
+//close rules box
+document.querySelector(DOMStrings.closeBtn).addEventListener("click", () => {
+  document.querySelector(DOMStrings.ruleBox).classList.remove("active");
+});
