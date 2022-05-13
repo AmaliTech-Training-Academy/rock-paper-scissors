f4e592503f905b9a01d3b7fe0e0938666e82cbca
Joseph Donkoh <93696965+JosephDonkoh@users.noreply.github.com>
2022-05-13 12:09:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-
+const scoreLabel = document.querySelector(".value");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+let rock = document.getElementById("rock");
+const gameOptions = document.querySelector(".game-options");
+const resultContainer = document.querySelector(".result-container");
+const resetBtn = document.querySelector(".play-again");
+const userImg = document.getElementById("user");
+const compImg = document.getElementById("machine");
+const buttons = document.querySelectorAll(".button");
+const player = document.getElementById("player");
+const computer = document.getElementById("computer");
+const resultBox = document.querySelector(".result-title");
+const hideComputerChoice = document.querySelector(".invisible");
+let gameResult = document.getElementById("status");
+let userChoice, computerChoice;
+let score = 0;
+let winner;
+let storedScore = 0;
